ef576110cb08ad4a50dde649f217d49135ee27ec
CaseyCrumb <caseyncrumb@gmail.com>
2023-08-27 16:55:18 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+var form = document.getElementById('todo-form');// Get the HTML element with the id 'todo-form' and store it in the 'form' variable.
+var input = document.getElementById('todo-input'); // Get the HTML element with the id 'todo-input' and store it in the 'input' variable.
+var list = document.getElementById('todo-list'); // Get the HTML element with the id 'todo-list' and store it in the 'list' variable.
+
